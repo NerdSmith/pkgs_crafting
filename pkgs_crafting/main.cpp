eb0ce9a2c5bb2ff5b8b8cce4d914cb20807f1bcb
Nikita Merzlyakov <merzlyakov02@gmail.com>
2022-05-13 15:14:55 +0300

    added test pcap files

--- pkgs_crafting/main.cpp
+++ pkgs_crafting/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "stdlib.h"
 #include "PcapLiveDeviceList.h"
 #include "PcapFileDevice.h"
@@ -11,16 +12,33 @@
 #include "UdpLayer.h"
 #include "HttpLayer.h"
 #include "DnsLayer.h"
+#include "PayloadLayer.h"
+
+std::string toHexStr(std::string s) 
+{
+	std::stringstream ss;
+
+	for (const auto& item : s) {
+		ss << std::hex << int(item);
+	}
+
+	return ss.str();
+}
+
 
 int main() {
 
-	pcpp::EthLayer newEthernetLayer(pcpp::MacAddress("1c:69:7a:ae:ef:e3"), pcpp::MacAddress("aa:bb:cc:dd:ee"));
+	pcpp::EthLayer newEthernetLayer(pcpp::MacAddress("1c:69:7a:ae:ef:e4"), pcpp::MacAddress("aa:bb:cc:dd:ee"));
 
-	pcpp::IPv4Layer newIPLayer(pcpp::IPv4Address("192.168.1.1"), pcpp::IPv4Address("10.0.0.1"));
+	pcpp::IPv4Layer newIPLayer(pcpp::IPv4Address("254.149.4.142"), pcpp::IPv4Address("188.235.32.202"));
 	newIPLayer.getIPv4Header()->ipId = pcpp::hostToNet16(2000);
 	newIPLayer.getIPv4Header()->timeToLive = 64;
 
-	//pcpp::TcpLayer newTcpLayer(12345, 80);
+	pcpp::UdpLayer newTransportLayer(12345, 110);
+	// newTransportLayer.getTcpHeader()->windowSize = pcpp::hostToNet16(6);
+
+	pcpp::PayloadLayer newPayload(toHexStr("hello!"));
+
 	//newTcpLayer.getTcpHeader()->windowSize = pcpp::hostToNet16(6);
 	//newTcpLayer.getTcpHeader()->pshFlag = 1;
 	//newTcpLayer.getTcpHeader()->ackFlag = 1;
@@ -33,10 +51,10 @@ int main() {
 	//newTcpLayer.addTcpOptionAfter(pcpp::TcpOptionBuilder(pcpp::PCPP_TCPOPT_NOP, (uint16_t) 100));
 
 
-	pcpp::UdpLayer newUdpLayer(12345, 53);
+	//pcpp::UdpLayer newUdpLayer(12345, 53);
 
-	pcpp::DnsLayer newDnsLayer;
-	newDnsLayer.addQuery("www.ebay.com", pcpp::DNS_TYPE_A, pcpp::DNS_CLASS_IN);
+	//pcpp::DnsLayer newDnsLayer;
+	//newDnsLayer.addQuery("www.ebay.com", pcpp::DNS_TYPE_A, pcpp::DNS_CLASS_IN);
 
  // 	pcpp::HttpRequestLayer httpRequestLayer(
 	//	pcpp::HttpRequestLayer::HttpGET, 
@@ -54,8 +72,8 @@ int main() {
 
 	newPacket.addLayer(&newEthernetLayer);
 	newPacket.addLayer(&newIPLayer);
-	newPacket.addLayer(&newUdpLayer);
-	newPacket.addLayer(&newDnsLayer);
+	newPacket.addLayer(&newTransportLayer);
+	newPacket.addLayer(&newPayload);
 
 	newPacket.computeCalculateFields();
 
@@ -63,6 +81,18 @@ int main() {
 	writer2.open();
 	writer2.writePacket(*(newPacket.getRawPacket()));
 	writer2.close();
+
+	pcpp::PcapLiveDevice* dev = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByIp("192.168.28.108");
+	dev->open();
+
+	if (!dev->sendPacket(&newPacket))
+	{
+		std::cerr << "Couldn't send packet" << std::endl;
+		return 1;
+	}
+	
+
+	dev->close();
 
 	return 0;
 }
